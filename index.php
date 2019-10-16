29504b2956404c8216f8d711501de27e14d7c09c
ᴘᴜꜱʜᴘᴇɴᴅᴇʀ ꜱɪɴɢʜ <34910300+akaibull@users.noreply.github.com>
2019-10-16 10:41:31 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-echo  "hello world,yayayyaaaayyyyy still this is openshift testing procedure and now testing jenkins for the 6th time ...  \n";
+echo  "hello world,yayayyaaaayyyyy still this is openshift testing procedure and now testing jenkins for the 7th time ...  \n";
 
 ?>
 
